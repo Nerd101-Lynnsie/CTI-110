4da66e19e17ff1b409c8e7e75ffcf4bf685a9de1
Nerd101-Lynnsie <hzaria910@gmail.com>
2025-09-19 12:54:21 -0400

    Add files via upload

---
+++ p1lab1_hill.py
@@ -0,0 +1,10 @@
+
+# Zaria Hill 09/19/2025 P1LAB1 Create a program that allows the user to enter their first name and last name.
+
+print("Enter your first name:",  end="")
+user_firstname = input()
+
+print("Enter your last name:",  end="")
+user_lastname = input()
+
+print("Hello,", user_firstname, user_lastname, "! Welcome to CTI-110")
